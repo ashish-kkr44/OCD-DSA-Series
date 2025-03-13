5777e137b338bebee47bd60e541df5cc2f14c5ed
ashish-kkr44 <ashishashsingh2007@gmail.com>
2025-03-14 00:12:34 +0530

    Day 12 BS

--- Day 12(q3).java
+++ Day 12(q3).java
@@ -1,3 +1,6 @@
+// Question link : https://leetcode.com/problems/search-insert-position/description/
+//T.C. : O(log N) & S.C. : O(1)
+
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int low=0, high=nums.length-1;
